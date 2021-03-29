0be2ac41d681539cc0357c4d76813dbb4b099e93
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-03-28 21:44:15 -0300

    feat(config): config jest and supertest

---
+++ test/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable-next-line */
+const { resolve } = require('path')
+const root = resolve(__dirname, '..')
+/* eslint-disable-next-line */
+const rootConfig = require(`${root}/jest.config.js`)
+
+module.exports = {
+  ...rootConfig,
+  ...{
+    rootDir: root,
+    displayName: 'end2end-tests',
+    setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'],
+    testMatch: ['<rootDir>/test/**/*.test.ts'],
+  },
+}
