4e4ee3e9f55d7466f2aee929bacf7c2880e75174
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-03-30 00:04:36 -0300

    feat(interface): contract for user repository

---
+++ src/app/repositories/IUserRepository.ts
@@ -0,0 +1,6 @@
+import { UserEntity } from '../entities/user.entity'
+
+export interface IUserRepository{
+  findByEmail(email: string): Promise<UserEntity>;
+  save(user: UserEntity): Promise<void>
+}
